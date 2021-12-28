b8a1ad37ea75763ea07d8c95058fa0d6d8425426
Alex Tsvelev <russo.turist@gmail.com>
2021-12-28 21:33:57 +0700

    made Readme and finished script to working level

--- mv-me.sh
+++ mv-me.sh
@@ -1,29 +1,38 @@
 #!/usr/bin/env bash
 # replacing last slash in first arg
-
 search_dir=${1%/}
-regexp="Files$"
-srex="*Ex_*"
-[[ $search_dir =~ $srex ]] && echo "huy $BASH_REMATCH"
+BASEDIR=$(dirname "$0")
+fex='Files$'
+srex='(Ex_Files_)'
+[[ $search_dir =~ $srex ]] && rename_base=1
+for entry in "$search_dir"/*
+do
+    if [[ $entry =~ $fex ]]
+    then
+	np=$BASEDIR/$entry/
+	cd "$np"
 
-# then echo match
-#     #echo "we need to rename $BASH_REMATCH";
-# fi
- 
-# for entry in "$search_dir"/*
-	    
-# do
-#    # echo `expr "$entry" : '\(.*Exercise\ Files$\)'`
+	for dirs in "."/*
+	do
+	    fold=$(basename $dirs)
+	    newdir=../$fold
+	    echo "Moving $fold"
+	   # echo "mv -f $fold $newdir"
+	    `mv -f $fold $newdir`
+	done
 
-#     # case entry in
-#     # 	"Exercise\ Files") echo "I'll make a job";
-#     #  esac 
-#       echo "$entry"
-	       
-# done
-# for file in "$search_dir"/*; do
-#     echo $file
-#     if [[ $file =~ $regex ]]; then
-#         echo -e "Found file: $file"
-#     fi
-# done
+	echo "Moving up "
+	cd ".."
+	echo "and now delete old useless folder"
+	rmdir -p "Exercise Files"
+	pp=`pwd`
+        fi
+done
+cd ..
+if (($rename_base))
+then
+nn=`echo $(basename $pp) | awk '{print substr($0,10)}'`
+#echo "mv $search_dir $nn"
+mv $search_dir $nn
+echo "Job is done"
+fi
